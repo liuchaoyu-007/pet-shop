c1b6a51999e4d278c5a7485189356d44e0e46fd9
geity <meike>
2018-07-20 16:14:21 +0800

    登录注册静态界面完成

--- Backstagemanagementsystem/src/components/login.vue
+++ Backstagemanagementsystem/src/components/login.vue
@@ -1,19 +1,85 @@
 <template>
-  <div>
-    登陆界面
+  <div class="telog">
+    <div class="teloel">
+      <el-card class="box-card user">
+        <p style="text-align: center;margin: 10px;color: rgb(53, 63, 68);font-size:30px;">宠物邦-登录</p>
+        <el-form status-icon ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="用户名">
+            <el-input placeholder="请输入用户名" v-model="user" clearable>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input placeholder="请输入密码" v-model="pass" clearable>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="登录类型">
+              <el-radio-group v-model="type">
+                <el-radio label="门店用户"></el-radio>
+                <el-radio label="平台管理"></el-radio>
+              </el-radio-group>
+            </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="btn">登录</el-button>
+            <el-button @click="reg">注册</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
+    </div>
+
   </div>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      user: "",
-      password: ""
-    };
-  },
-  methods: {
-  }
-};
+  export default {
+    data() {
+      return {
+        user: "",
+        pass: "",
+        type:""
+      };
+    },
+    methods: {
+      btn() {
+        console.log(this)
+        if (this.user != "" && this.pass != "" && this.type != "") {
+          this.$router.push('/info')
+        } else {
+          this.alerts()
+        }
+      },
+      reg() {
+        this.$router.push('/reg')
+      },
+      alerts() {
+        this.$alert('输入错误', '提示', {
+          confirmButtonText: '确定',
+        });
+      }
+    }
+  };
 </script>
 <style>
+  .user {
+    width: 500px;
+    height: 400px;
+    box-shadow: 20 0 0 15 red;
+    margin: 100px auto;
+  }
+
+  .telog {
+    height: 615px;
+    background-image: url("../assets/16df68841428aaa063dd76639ca3cab8.jpg");
+    background-repeat: no-repeat;
+    background-size: 100%;
+  }
+
+  .teloel {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    background: rgba(0, 0, 0, .4);
+    width: 100%;
+    height: 100%;
+
+  }
 </style>
--- Backstagemanagementsystem/src/router.js
+++ Backstagemanagementsystem/src/router.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import Login from "./components/login.vue"//登陆界面
+import Login from "./components/login.vue"//登陆界面
 import Info from "./components/info.vue"//导航首页
+import Reg from './components/reg.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/reg',
+      name: 'reg',
+      component: Reg
+    },
+    {
+      path: '/info',
       name: 'info',
       component: Info
-    },
+    }
   ]
 })
